21292d4c9375ef714ade8ab1ece8499d074d938d
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-06-05 22:59:54 +0600

    Create Luv Basics.cpp

---
+++ Luv Basics.cpp
@@ -0,0 +1,38 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+//Title:-C++ Basics for Competitive Programming - Part 1 | CP Course | EP 2
+
+benifit of bits/stdc++.h
+
+Basic Structure and special headers file : (0:00)
+Understanding Cout : (2:58)
+Variables : (3:21)
+Datatypes : (3:59)
+Simple Operators : (7:29)
+Modulo Operator : (7:59)
+Understanding ++ and -- : (8:44)
+Relational Operators : (11:39)
+Characters and ascii : (12:05)
+Understanding cin : (14:31)
+
+Overflow, Precision Errors, Calculation Order | C++ Basics - Part 2 | CP Course | EP 3
+
+Topics to be covered : (0:00)
+Calculation Order in Datatypes : (0:29)
+Operator Precedence : (3:24)
+Overflow : (6:08)
+Precision Error in Double and Float : (12:11)
+Solving your first programming question : (15:42)
+
+-10^9 <int> 10^9
+
+-10^12 <long int> 10^12
+
+-10^18 <long long int> 10^18
+
+
+cout<<fixed<<setprecision(4)<<endl;
